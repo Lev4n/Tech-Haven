567b826e328428fbdcefd5245018010be51f82d2
Levan Lomidze <levan.lomo@gmail.com>
2023-12-18 19:07:03 +0400

    fixed import bugs

--- src/app/(pages)/account/orders/page.tsx
+++ src/app/(pages)/account/orders/page.tsx
@@ -38,7 +38,7 @@ export default async function Orders() {
       })
       ?.then(json => json.docs)
   } catch (error) {
-    console.error(error)
+    console.error('An error occurred:', error);
   }
 
   return (
--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Metadata } from 'next'
-import Link from 'next/link'
 import Image from 'next/image'
+import Link from 'next/link'
 
 import { Gutter } from '../../_components/Gutter'
 import { RenderParams } from '../../_components/RenderParams'
